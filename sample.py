96cb216e9658c55b6d8ad596240dce748bfa6e5b
Wentao Dai <weda@microsoft.com>
2020-01-03 19:50:04 +0800

    Add more detailed implementation

--- sample.py
+++ sample.py
@@ -1,15 +1,12 @@
-from new_module_sdk import Workspace, NewDataset, NewModuleStep, submit_experiment
+from new_module_sdk import Workspace, DatasetX, ModuleStepX, ExperimentX
+
 
 if __name__ == '__main__':
-    ws = Workspace.get(
-        name='your-name',
-        resource_group='your-rg',
-        subscription_id='your-sub'
-    )
-    dataset = NewDataset.get(ws, "GenericCSV/Automobile_price_data_(Raw)")
+    ws = Workspace.from_config()
+    dataset = DatasetX.get_by_data_reference(ws, "GenericCSV/Automobile_price_data_(Raw)")
     module_name = 'azureml://Convert to Dataset'
-    step1 = NewModuleStep.get(ws, name=module_name)
+    step1 = ModuleStepX.get(ws, name=module_name)
     step1.Dataset = dataset
-    step2 = NewModuleStep.get(ws, name=module_name)
+    step2 = ModuleStepX.get(ws, name=module_name)
     step2.Dataset = step1.Results_dataset
-    submit_experiment(ws, [step1, step2], 'test_experiment')
+    ExperimentX.submit(ws, [step1, step2], 'test_experiment_ex')
